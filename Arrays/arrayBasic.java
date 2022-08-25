e60ca3ccf93632d640794bc593ecc72dc67a686b
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-08-25 17:24:56 +0530

    Basic of Array in Java

---
+++ Arrays/arrayBasic.java
@@ -0,0 +1,57 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class arrayBasic {
+    
+    public static void main(String[] args){
+
+        int[] Intarr = new int[5];
+        char[] charArr = new char[5]; 
+        boolean[] boolArr = new boolean[5];
+
+        // Taking Input in Java
+        Scanner inp = new Scanner(System.in);
+
+
+        // // Taking Input Integer in Java (Array)
+        // for(int j = 0; j < Intarr.length; j++){
+        //     System.out.println("Index : " + j);
+        //     Intarr[j] = inp.nextInt();
+        // }
+
+        // // Printing
+        // for(int i = 0; i < Intarr.length; i++){
+        //     System.out.print(Intarr[i] + " ");
+        // }
+
+
+        // // Taking Character Input in java (Array)
+        // for(int j = 0; j < charArr.length; j++){
+        //     System.out.println("Char at : " + j);
+        //     charArr[j] = inp.next().charAt(0);
+
+        // }
+        // // Printing
+        // for(int i = 0; i < charArr.length; i++){
+        //     System.out.print(charArr[i] + " ");
+        // }
+        // System.out.println();
+
+
+        // Taking Input as Boolean in java
+        // Only take true or false
+        for(int j = 0; j < boolArr.length; j++){
+            System.out.println("Boolean : " + j);
+            boolArr[j] = inp.nextBoolean();
+        }
+
+        // Printing
+        for(int i = 0; i < boolArr.length; i++){
+            System.out.print(boolArr[i] + " ");
+        }
+
+
+
+    }
+}
