d2a9c32fa68c027c92f3e5ced6f6a21b49aff000
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-08-25 23:47:34 +0530

    Array is Sorted or not Optimized

---
+++ Arrays/isSorted.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class isSorted {
+
+    public static void main(String[] args){
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+
+        boolean is = false;
+        
+        for(int i = 0; i < arr.length-1; i++){
+            int j = i+1;
+            
+            if(arr[i] < arr[j]){
+                is = true;
+            }
+            else{
+                is = false;
+                break;
+            }
+
+        }
+
+        if(is){
+            System.out.println("Sorted");
+        }else{
+            System.out.println("Unsorted");
+        }
+    }
+    
+}
