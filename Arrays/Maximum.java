aefbcd62b325b40abf45230cd7743d637ef7c670
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-08-25 17:51:35 +0530

    Find maximum from the array.

---
+++ Arrays/Maximum.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.Scanner;
+
+public class Maximum {
+    public static void main(String[] args){
+
+        int[] arr = new int[7];
+
+
+        Scanner inp = new Scanner(System.in);
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print("Element at Index-" + i + " = ");
+            arr[i] = inp.nextInt();
+        }
+
+        int maximum = 0;
+
+        for(int j = 0; j < arr.length; j++){
+
+            if(arr[j] > maximum){
+                maximum = arr[j];
+            }
+        }
+
+        System.out.println("The Maximum Element in Array is : " + maximum);
+
+    }
+}
