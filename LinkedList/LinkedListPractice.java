3658ad941e68bfc6e251c7b0feba6e71da8642b7
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-08-25 10:52:21 +0530

    first commit

---
+++ LinkedList/LinkedListPractice.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+class LinkedListPractice{
+    // if we want to store two things we use class in java
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Array - continuous memory allocation
+
+// LinkedList - they are not continuous they can be differnt memory location
+// but all the nodes differnt memory location but they are Linked
+
+// there are two things are stored in Linkedlist one is data and other is address of next element(Node)
+// data, addressOfNextNode
+
+// everything in linked list is called as node.
+
+// Head will point to first node
+// Head stores address of first node
+
+// Tail will point to last node
+// Tail is address of last node
---
+++ LinkedList/NodeObject.java
@@ -0,0 +1,43 @@
+package LinkedList;
+
+public class NodeObject {
+    public static void main(String[] args){
+        Node head = new Node(5);
+        
+        Node secondData = new Node(6);
+
+        Node thirdData = new Node(7);
+
+        Node fourthData = new Node(8);
+
+        head.next = secondData;
+        secondData.next = thirdData;
+        thirdData.next = fourthData;
+
+
+        // These are the address which in stack memory
+        // System.out.println(head.next);
+        // System.out.println(head.next);
+        // System.out.println(secondData);
+        // System.out.println(thirdData);
+        // System.out.println(fourthData);
+
+        printLinkedList(head);
+
+    }
+
+    public static void printLinkedList(Node head){
+
+        // System.out.println(head.data);
+        // System.out.println(head.next.data);
+        // System.out.println(head.next.next.data);
+        // System.out.println(head.next.next.next.data);
+
+        while(head != null){
+            System.out.println(head.data);
+            head = head.next;
+        }
+
+        // System.out.println(head);
+    }
+}
